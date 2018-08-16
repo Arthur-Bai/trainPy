270afe5b5e17a41be27c430bab52dd233a42aa52
Arthur_Bai <18610940312@126.com>
2018-08-16 10:13:24 +0800

    JFrog

--- testArtifactory.py
+++ testArtifactory.py
@@ -1,6 +1,5 @@
 from artifactory import ArtifactoryPath
-path = ArtifactoryPath(
-    "https://na.artifactory.swg-devops.com/artifactory/txo-dswim-esb-deployment-generic-virtual",
-    auth=('baiyx@cn.ibm.com', 'AP6LE3b5ML1LjbFAWiXjZtnpsUr'))
-for p in path:
-    print(p)
+path = ArtifactoryPath("https://na.artifactory.swg-devops.com/artifactory/txo-dswim-esb-deployment-generic-local/Train-0.3/")
+
+path.mkdir()
+path.deploy_file('box.py')
